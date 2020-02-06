ea6ac2e57bc516537e8756a1940d8d2d713e6e48
david-res <52652879+david-res@users.noreply.github.com>
2020-02-06 17:16:03 +0200

    Update timingTest.ino
    Added MB support and Interrupt

--- timingTest.ino
+++ timingTest.ino
@@ -1,37 +1,164 @@
-const long loopDelay1 = 10;
 
-unsigned long timeNow1 = 0;
-float canData [7]; //place up to date CAN data into this array
-int boost_percent;
-float boost_pressuer, boost_final;
 
-void loop(void) {
-  
-  if(millis() > timeNow1 + loopDelay1){
-    timeNow1 = millis();
-    elapsedMillis em = 0; 
-    canData [0] = PID_01(INTAKE_MANIFOLD_ABSOLUTE_PRESSURE); //BOOST
-    canData [2] = PID_01(ABSOLULTE_BAROMETRIC_PRESSURE);
-    Serial.printf("elapsed PID loop poll %u\n", (uint32_t)em);
-    em = 0;
+#include "SPI.h"
+#include <FlexCAN_T4.h>
+#include <ILI9341_t3n.h>
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
 
+#define COLOR_RED   ILI9341_RED
+#define COLOR_GREEN ILI9341_GREEN
+#define COLOR_BLUE  ILI9341_BLUE
+#define COLOR_YELLOW ILI9341_YELLOW
+#define COLOR_BLACK ILI9341_BLACK
+
+//
+#include "defi_n_150.c" //the picture
+// Normal Connections
+//#define USE_SPI1
+//#define USE_SPI2
+//-----------------------------
+// SPI1
+//-----------------------------
+#ifdef USE_SPI1
+#ifdef __IMXRT1062__
+#define TFT_DC       9
+#define TFT_CS      10
+#define TFT_RST     8  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    26
+#define TFT_SCLK    27
+#define TFT_MISO    1
+#else
+#define TFT_DC      31
+#define TFT_CS      10
+#define TFT_RST     8  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    0
+#define TFT_SCLK    32
+#define TFT_MISO    1
+#endif
+//-----------------------------
+// SPI2
+//-----------------------------
+#elif defined(USE_SPI2)
+#ifdef __IMXRT1062__
+#define TFT_DC       9
+#define TFT_CS      10
+#define TFT_RST     8  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    35
+#define TFT_SCLK    37
+#define TFT_MISO    34
+#else
+#define TFT_DC      43
+#define TFT_CS      10
+#define TFT_RST     8  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    44
+#define TFT_SCLK    46
+#define TFT_MISO    45
+#endif
+//-----------------------------
+// SPI - Default
+//-----------------------------
+#else
+#define TFT_DC       9
+#define TFT_CS      10
+#define TFT_RST    255  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    11
+#define TFT_SCLK    13
+#define TFT_MISO    12
+#endif
+
+ILI9341_t3n tft = ILI9341_t3n(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+
+enum {
+    INTAKE_MANIFOLD_ABSOLUTE_PRESSURE = 0x0b,
+    ABSOLULTE_BAROMETRIC_PRESSURE = 0x33,
+   
+};
+
+//colors
+#define background 0x2945
+
+
+
+uint16_t g_center_x;
+uint16_t g_center_y;
+uint16_t g_offset_x;
+uint16_t g_offset_y;
+
+int16_t g_needle_rect_min_x;
+int16_t g_needle_rect_min_y;
+int16_t g_needle_rect_max_x;
+int16_t g_needle_rect_max_y;
+
+void setup() {
+  while (!Serial && millis() < 5000) ;
+  Serial.begin(115200);
+  //Serial.printf("CS:%d, DC:%d RST:%d MOSI:%d, SCLK:%d MISO:%d\n", TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+
+ 
+  Can0.begin();
+  Can0.setBaudRate(500000);
+  Can0.setMBFilter(MB0,0x7E8);
+  Can0.setMBFilter(MB1,0x7E8);
+  Can0.setMBFilter(MB2,0x7E8);
+  Can0.enableMBInterrupt(MB0);
+  Can0.enableMBInterrupt(MB1);
+  Can0.enableMBInterrupt(MB2);
+  Can0.onReceive(canSniff);
+
+
+
+
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(COLOR_RED);
+  tft.fillScreen(COLOR_GREEN);
+  tft.fillScreen(COLOR_BLUE);
+  delay(100);
+  tft.fillScreen(COLOR_BLACK);
+  g_center_x = tft.width() / 4;
+  g_center_y = tft.height() / 2;
+
+  g_offset_x = 5; //(tft.width() - 150) / 2;
+  g_offset_y = 45; //(tft.height() - 150) / 2;
+  Serial.printf("center(%u, %u) offset(%u %u)\n", g_center_x, g_center_y, g_offset_x, g_offset_y);
+  tft.writeRect(g_offset_x, g_offset_y, 150, 150, (const uint16_t*)defi);
+  tft.useFrameBuffer(true);
+  tft.fillScreen(COLOR_BLACK);
 }
 
-elapsedMillis em2 = 0; 
 
-boost_pressuer = (canData [0] - 102);
-boost_final = boost_pressuer * 0.145038;
+const long loopDelay1 = 10;
+const long loopDelay2 = 500;
+unsigned long timeNow1, timeNow2 = 0;
+float canData [2]; //place up to date CAN data into this array
+
+int boost_percent;
+float boost_pressuer, boost_final;
+float afr;
+
+void loop(void) {
+    if(millis() > timeNow1 + loopDelay1){
+        timeNow1 = millis();
+        elapsedMillis em = 0; 
+        canSend(INTAKE_MANIFOLD_ABSOLUTE_PRESSURE);
+        canSend(ABSOLULTE_BAROMETRIC_PRESSURE);
+        Serial.printf("elapsed PID loop poll %u\n", (uint32_t)em);
+        em = 0;
+    }
+
+        elapsedMillis em2 = 0; 
+        boost_pressuer = (canData [0] - 102);
+        boost_final = boost_pressuer * 0.145038;
+
+
     if (boost_pressuer < 0 ){
-    boost_percent = map(boost_pressuer, -110, -0.99,0,249);
+        boost_percent = map(boost_pressuer, -110, -0.99,0,249);
     }
 
     if (boost_pressuer >= 0.1) {
-    boost_percent = map(boost_pressuer, 0.1, 200.0, 250, 750);
+        boost_percent = map(boost_pressuer, 0.1, 200.0, 250, 750);
     }
 
-    if (canData [1] > 22){
-    canData [1] = 22;
-    }
 
     int16_t min_x = g_needle_rect_min_x;
     int16_t min_y = g_needle_rect_min_y;
@@ -56,202 +183,78 @@ boost_final = boost_pressuer * 0.145038;
     tft.setClipRect();
     Serial.printf("elapsed Screen Update %u\n", (uint32_t)em2);
     em2 = 0;
+
 }
 
-//MODE 01 PID'S
-float PID_01(uint8_t pid) 
-{
-  elapsedMillis em1 = 0; 
 
-  CAN_message_t can_MsgRx,can_MsgTx;
+void canSend(uint16_t pid){  //Send the PID requests
+  elapsedMillis em1 = 0; 
+  CAN_message_t msgTx, msgRx;
   
-  can_MsgTx.buf[0] = 0x02;  // Two bytes in the request
-  can_MsgTx.buf[1] = 0x01; // OBD mode 1
-  can_MsgTx.buf[2] = pid;  
-  can_MsgTx.buf[3] = 0;
-  can_MsgTx.buf[4] = 0;  
-  can_MsgTx.buf[5] = 0;
-  can_MsgTx.buf[6] = 0;  
-  can_MsgTx.buf[7] = 0;
-  can_MsgTx.len = 8; // number of bytes in request
-  //can_MsgTx.ext = 0; 
-  can_MsgTx.flags.extended = 0; // 11 bit header, not 29 bit
-  can_MsgTx.flags.remote = 0;
-  can_MsgTx.id = 0x7E0; // request header for OBD
-//  can_MsgTx.timeout = 500;
-  Can0.write(can_MsgTx);  
+  msgTx.buf[0] = 0x02;  // Two bytes in the request
+  msgTx.buf[1] = 0x01; // OBD mode 1
+  msgTx.buf[2] = pid;  
+  msgTx.buf[3] = 0;
+  msgTx.buf[4] = 0;  
+  msgTx.buf[5] = 0;
+  msgTx.buf[6] = 0;  
+  msgTx.buf[7] = 0;
+  msgTx.len = 8; // number of bytes in request
+  msgTx.flags.extended = 0; // 11 bit header, not 29 bit
+  msgTx.flags.remote = 0;
+  msgTx.id = 0x7E0; // request header for OBD
+
+  Can0.write(msgTx);  
 
   Serial.printf("elapsed TX %u\n", (uint32_t)em1);
   em1 = 0;
-
-  elapsedMillis waiting;     // "waiting" starts at zero
-  
-  while (waiting < 20) {   //Check for timeout
-
-    if(Can0.read(can_MsgRx)) { 
+}
 
 
-                    #define A can_MsgRx.buf[3]
-                    #define B can_MsgRx.buf[4]
-                    #define C can_MsgRx.buf[5]
-                    #define D can_MsgRx.buf[6]
+void canSniff(const CAN_message_t &msg) { // global callback
 
-        if((can_MsgRx.id == 0x7E8) && (can_MsgRx.buf[2] == pid))
-       {
-                    //uint16_t pid_n = (uint16_t)(message.data[2] << 8) | message.data[3];
-                    switch (pid){
+    #define res_count msg.buf[0] // numper of bytes returned
+    #define req_mode msg.buf[1] // mode 0x01 or 0x22
+    #define a msg.buf[2] // PID
+    #define b msg.buf[3] // res1 or PID (for mode 0x22)
+    #define c msg.buf[4] // res 2 or 1
+    #define d msg.buf[5] // res 3 or 2
+    #define e msg.buf[6] // res 4 or 3
+    #define f msg.buf[7] // res 5 or 4
 
-                    //case FUEL_SYSTEM_STATUS: // raw
-                   case RUN_TIME_SINCE_ENGINE_START:
-                    //case DISTANCE_TRAVELED_WITH_MIL_ON:
-                    //case DISTANCE_TRAVELED_SINCE_CODES_CLEARED:
-                    //case TIME_RUN_WITH_MIL_ON:
-                    //case TIME_SINCE_TROUBLE_CODES_CLEARED:
-                        return (A * 256.0 + B);
-                        break;
+    if(req_mode == 0x01){
+          switch (a){
+            case INTAKE_MANIFOLD_ABSOLUTE_PRESSURE:
+            canData [0] = b;
+            break;
+            
+            case ABSOLULTE_BAROMETRIC_PRESSURE:
+            canData [1] = b;
+            break;                  
+        }
+      }
 
-                    //case CALCULATED_ENGINE_LOAD:
-                    case THROTTLE_POSITION:
-                    //case COMMANDED_EGR:
-                    //case COMMANDED_EVAPORATIVE_PURGE:
-                    //case FUEL_TANK_LEVEL_INPUT:
-                    case RELATIVE_THROTTLE_POSITION:
-                    //case ABSOLUTE_THROTTLE_POSITION_B:
-                    //case ABSOLUTE_THROTTLE_POSITION_C:
-                    //case ABSOLUTE_THROTTLE_POSITION_D:
-                    //case ABSOLUTE_THROTTLE_POSITION_E:
-                    //case ABSOLUTE_THROTTLE_POSITION_F:
-                    //case COMMANDED_THROTTLE_ACTUATOR:
-                    //case ETHANOL_FUEL_PERCENTAGE:
-                    //case RELATIVE_ACCELERATOR_PEDAL_POSITTION:
-                    //case HYBRID_BATTERY_PACK_REMAINING_LIFE:
-                        return (A / 2.55);
-                        break;
+}
 
-                    //case COMMANDED_SECONDARY_AIR_STATUS:                    // raw
-                    //case OBD_STANDARDS_THIS_VEHICLE_CONFORMS_TO:            // raw
-                    //case OXYGEN_SENSORS_PRESENT_IN_2_BANKS:                 // raw
-                    //case OXYGEN_SENSORS_PRESENT_IN_4_BANKS:                 // raw
-                    //case AUXILIARY_INPUT_STATUS:                            // raw
-                    //case FUEL_TYPE:                                         // raw
-                    //case EMISSION_REQUIREMENT_TO_WHICH_VEHICLE_IS_DESIGNED: // raw
-                        //return (A);
 
-                    case OXYGEN_SENSOR_1_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_2_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_3_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_4_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_5_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_6_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_7_SHORT_TERM_FUEL_TRIM:
-                    case OXYGEN_SENSOR_8_SHORT_TERM_FUEL_TRIM:
-                        return ((B / 1.28) - 100.0);
-                        break;
-
-                    case ENGINE_COOLANT_TEMPERATURE:
-                    case AIR_INTAKE_TEMPERATURE:
-                    case AMBIENT_AIR_TEMPERATURE:
-                    //case ENGINE_OIL_TEMPERATURE:
-                        return (A - 40.0);
-                        break;
-/*
-                    case SHORT_TERM_FUEL_TRIM_BANK_1:
-                    case LONG_TERM_FUEL_TRIM_BANK_1:
-                    case SHORT_TERM_FUEL_TRIM_BANK_2:
-                    case LONG_TERM_FUEL_TRIM_BANK_2:
-                    case EGR_ERROR:
-                        return ((A / 1.28) - 100.0);
-*/
-                    case FUEL_PRESSURE:
-                        return (A * 3.0);
-                        break;
-
-                    case INTAKE_MANIFOLD_ABSOLUTE_PRESSURE:
-                    //ase VEHICLE_SPEED:
-                    //case WARM_UPS_SINCE_CODES_CLEARED:
-                    case ABSOLULTE_BAROMETRIC_PRESSURE:
-                        return (A);
-                        break;
-
-                    case ENGINE_RPM:
-                        return ((A * 256.0 + B) / 4.0);
-                        break;
-
-                    case TIMING_ADVANCE:
-                        return ((A / 2.0) - 64.0);
-                        break;
-
-                    //case MAF_AIR_FLOW_RATE:
-                      //  return ((A * 256.0 + B) / 100.0);
-
-                    //case FUEL_RAIL_PRESSURE:
-                      //  return ((A * 256.0 + B) * 0.079);
-
-                    //case FUEL_RAIL_GAUGE_PRESSURE:
-                    //case FUEL_RAIL_ABSOLUTE_PRESSURE:
-                      //  return ((A * 256.0 + B) * 10.0);
-
-                    case OXYGEN_SENSOR_1_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_2_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_3_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_4_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_5_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_6_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_7_FUEL_AIR_EQUIVALENCE_RATIO:
-                    case OXYGEN_SENSOR_8_FUEL_AIR_EQUIVALENCE_RATIO:
-                        return (((A * 256.0 + B) * 2.0) / 65536.0);
-                        break;
-
-                   // case EVAP_SYSTEM_VAPOR_PRESSURE:
-                     //   return (((int16_t)(A * 256.0 + B)) / 4.0);
-
-                    case CATALYST_TEMPERATURE_BANK_1_SENSOR_1:
-                    case CATALYST_TEMPERATURE_BANK_2_SENSOR_1:
-                    case CATALYST_TEMPERATURE_BANK_1_SENSOR_2:
-                    case CATALYST_TEMPERATURE_BANK_2_SENSOR_2:
-                        return (((A * 256.0 + B) / 10.0) - 40.0);
-                        break;
-
-                    case CONTROL_MODULE_VOLTAGE:
-                        return ((A * 256.0 + B) / 1000.0);
-                        break;
-
-                    //case ABSOLUTE_LOAD_VALUE:
-                      //  return ((A * 256.0 + B) / 2.55);
-
-                    case FUEL_AIR_COMMANDED_EQUIVALENCE_RATE:
-                        return (2.0 * (A * 256.0 + B) / 65536.0);
-                        break;
-
-                    //case ABSOLUTE_EVAP_SYSTEM_VAPOR_PRESSURE:
-                      //  return ((A * 256.0 + B) / 200.0);
-
-                    //case 0x54:
-                      //  return ((A * 256.0 + B) - 32767.0);
-
-                    //case FUEL_INJECTION_TIMING:
-                      //  return (((A * 256.0 + B) / 128.0) - 210.0);
-
-                    //case ENGINE_FUEL_RATE:
-                      //  return ((A * 256.0 + B) / 20.0);
-                    }
-                }
-
-                Serial.printf("elapsed RX %u\n", (uint32_t)em1);
-                em1 = 0;
-
-                if (can_MsgRx.buf[1] == 0x7F)
-                {
-                    return NAN;
-                }  
-
-                
-         }
-         
-
- 
-  } // while
-  
-  return 0;
-
+const double NEEDLE_LEN = 60;
+const double TRI_HALF_WIDTH = 2;
+void drawNeedle(int percent, uint16_t color) {
+  double rads = HALF_PI + TWO_PI * percent/ 1000.0;
+  uint16_t x0 = cos(rads) * NEEDLE_LEN + g_center_x;
+  uint16_t y0 = sin(rads) * NEEDLE_LEN + g_center_y;
+  uint16_t x1 = cos(rads - HALF_PI) * TRI_HALF_WIDTH + g_center_x;
+  uint16_t y1 = sin(rads - HALF_PI) * TRI_HALF_WIDTH + g_center_y;
+  uint16_t x2 = cos(rads + HALF_PI) * TRI_HALF_WIDTH + g_center_x;
+  uint16_t y2 = sin(rads + HALF_PI) * TRI_HALF_WIDTH + g_center_y;
+  /*
+    Serial.print("Percent: ");Serial.print(percent, 3);
+    Serial.print("Rads: ");Serial.print(rads, 3);
+    Serial.printf(" (%u, %u) (%u %u) (%u %u)\n", x0, y0, x1, y1, x2, y2);
+  */
+  tft.fillTriangle(x0, y0, x1, y1, x2, y2, color);
+  g_needle_rect_min_x = min(min(x0, x1), x2);
+  g_needle_rect_min_y = min(min(y0, y1), y2);
+  g_needle_rect_max_x = max(max(x0, x1), x2);
+  g_needle_rect_max_y = max(max(y0, y1), y2);
 }
